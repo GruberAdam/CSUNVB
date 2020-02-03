5e9e5441b25a1d4f1cefe7d3aa74ab48321935ef
JanBlatterCPNV <48351896+JanBlatterCPNV@users.noreply.github.com>
2020-02-03 12:15:48 +0100

    mise  à jour d'une ligne

--- view/drugHome.php
+++ view/drugHome.php
@@ -5,8 +5,9 @@ $title = "CSU-NVB - Stupéfiants";
 <div class="row m-2">
     <h1>Stupéfiants</h1>
     <table>
-        <thead><tr><th></th><th>Col 1</th></tr></thead>
+        <thead><tr><th></th><th>Stupéfiants</th></tr></thead>
         <tbody><tr><td>Ligne 1</td><td onclick="changeColor(this)"></td></tr></tbody>
+
     </table>
 </div>
 <?php

bb7e7c607c4578133d77569d5996e68c4be79b6b
JessyBorcard <48352287+JessyBorcard@users.noreply.github.com>
2020-02-03 11:32:10 +0100

    First Commit
    That's just a test, no worries.

--- view/drugHome.php
+++ view/drugHome.php
@@ -4,6 +4,11 @@ $title = "CSU-NVB - Stupéfiants";
 ?>
 <div class="row m-2">
     <h1>Stupéfiants</h1>
+
+    <table>
+        <thead><tr><th>Test</th></tr></thead>
+        <tbody><tr><td>Oui Oui</td></tr></tbody>
+    </table>
 </div>
 <?php
 $content = ob_get_clean();

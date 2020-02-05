8b06af4395ca50611edc905c1b2233ecd41bbe9b
JessyBorcard <48352287+JessyBorcard@users.noreply.github.com>
2020-02-05 14:49:50 +0100

    test
    test

--- view/drugHome.php
+++ view/drugHome.php
@@ -4,10 +4,12 @@ $title = "CSU-NVB - Stupéfiants";
 ?>
 <div class="row m-2">
     <h1>Stupéfiants</h1>
+    <form>
     <table>
         <thead><tr><th></th><th>Col 1</th></tr></thead>
         <tbody><tr><td>Ligne 1</td><td onclick="changeColor(this)"></td></tr></tbody>
     </table>
+    </form>
 </div>
 <?php
 $content = ob_get_clean();

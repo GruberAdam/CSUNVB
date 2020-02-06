e32b7dc0c866b1fcae7a6a25111c9c9177368c45
JanBlatterCPNV <48351896+JanBlatterCPNV@users.noreply.github.com>
2020-02-06 14:38:29 +0100

    création de tableau

--- view/drugHome.php
+++ view/drugHome.php
@@ -3,11 +3,25 @@ ob_start();
 $title = "CSU-NVB - Stupéfiants";
 ?>
 <div class="row m-2">
-    <h1>Stupéfiants</h1>
+    <h1>Contrôle de stupéfiants Hebdomadaire</h1>
     <form>
     <table>
         <thead><tr><th></th><th>Stupéfiants</th></tr></thead>
-        <tbody><tr><td>Ligne 1</td><td onclick="changeColor(this)"></td></tr></tbody>
+        <tbody><tr>
+            <td>Morphine N*</td>
+            <?php
+            for($compteur=1;$compteur<63;$compteur++){
+                echo "<td>[___]</td>";
+            }
+            ?>
+        </tr></tbody>
+        <tbody><tr><td> ...</td>
+            <?php
+            for($compteur=1;$compteur<63;$compteur++){
+                echo "<td>[___]</td>";
+            }
+            ?>
+        </tr></tbody>
 
     </table>
     </form>

85c5c4d6c34201994a1ad56406990a6ff452cac4
GRUBER Adam <Adam.GRUBER@cpnv.ch>
2020-02-10 10:28:39 +0100

    We added the home page when the login worked. Admins are added in the json

--- view/loginCongratulations.php
+++ view/loginCongratulations.php
@@ -12,6 +12,10 @@ ob_start();
 <div class="alert alert-success" role="alert">
     <h4 class="alert-heading">Connexion réussie!</h4>
     <p>Vous êtes bien connecté sur <?php echo "<b> $_SESSION[mail]</b>" ?></p>
+    <hr>
+    <div class="alert alert-success" role="alert">
+        Revenir a la page <a href="index.php?action=home" class="alert-link">d'acceuil</a>.
+    </div>
 </div>
 
 <?php

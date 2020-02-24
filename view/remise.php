0db3b27ef702e23d62e046b0872ea02176580cbe
EAASWARALINGAM Kaarththigan <Kaarththigan.EAASWARALINGAM@cpnv.ch>
2020-02-24 12:16:00 +0100

    Remise : Début du formulaire

--- view/remise.php
+++ view/remise.php
@@ -1,1 +1,40 @@
 <?php
+ob_start()
+
+?>
+    <div class="container" style="margin-top: 10px">
+        <div class="row">
+            <div class="col-1">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="customCheck1">
+                    <label class="custom-control-label" for="customCheck1">Oui</label>
+                </div>
+            </div>
+            <div class="col-3">
+                <select class="custom-select">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+            <div class="col-1">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="customCheck2">
+                    <label class="custom-control-label" for="customCheck2">Non</label>
+                </div>
+            </div>
+            <div class="col-3">
+                <select class="custom-select">
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+        </div>
+    </div>
+<?php
+$content = ob_get_clean();
+require "gabarit.php";
+?>

e74738d70be5805d82463f81e5e6fec482b23152
JanBlatterCPNV <48351896+JanBlatterCPNV@users.noreply.github.com>
2020-02-27 16:28:35 +0100

    maj

--- view/drugHome.php
+++ view/drugHome.php
@@ -8,8 +8,8 @@ $title = "CSU-NVB - Stupéfiants";
     <h1>Contrôle de stupéfiants Hebdomadaire</h1><br>
     <a href="index.php?action=drugAdd"><button class="btn btn-primary m-1 pull-right">Ajouter</button></a>
     <form>
-        <table class="input">
-            <tbody><tr >
+        <table class="">
+            <tbody ><tr >
 
                 <?php
 
@@ -24,14 +24,15 @@ $title = "CSU-NVB - Stupéfiants";
 
                 ?>
                 <?php
-                $compteur3 = 0;
+                $compteur3 = 1;
                 $compteur4 = 0;
                 $data = $_POST;
                 var_dump($_POST);
-                for($compteur=0;$compteur<14;$compteur++){
+                echo " <td id='rowh1cel0' height='100' ><p class='' onmouseover='getDate(this)' ></p></td>";
+                for($compteur=1;$compteur<15;$compteur++){
 
                     if ($compteur==$compteur3){
-                        echo " <td id='rowh1cel$compteur3' height='100'><p class='sideway center' onmouseover='getDate(this)'></p></td>";
+                        echo " <td id='rowh1cel$compteur3' height='100' colspan='4'><p class='  FirstCase' onmouseover='getDate(this)' ></p></td>";
                         $compteur3 = $compteur3 + 2;
                     }
 
@@ -41,9 +42,12 @@ $title = "CSU-NVB - Stupéfiants";
                 }
                 ?>
             </tr>
+
             <tr>
+
                 <?php
-                for($compteur2=0;$compteur2<29;$compteur2++){
+                echo "<td id='h2cel0' class='input'><p id='ph2cel$compteur' class='input'></p></td>";
+                for($compteur2=1;$compteur2<30;$compteur2++){
 
 
 
@@ -63,10 +67,10 @@ $title = "CSU-NVB - Stupéfiants";
                 for ($compteur = 0; $compteur < 29; $compteur++) {
 
                     if(!isset( $data["inputlig{$compteur5}cel{$compteur}"])){
-                        echo "<td id='lig{$compteur5}cel{$compteur}' ><input type='text' name='inputlig{$compteur5}cel{$compteur}'  class='input' value='0'></td>";
+                        echo "<td id='lig{$compteur5}cel{$compteur}' ><input type='text' name='inputlig{$compteur5}cel{$compteur}'  class='Case' value='0'></td>";
                     }
                     else {
-                        echo "<td id='lig{$compteur5}cel{$compteur}' ><input type='text' name='inputlig{$compteur5}cel{$compteur}'  class='input' value='{$data["inputlig{$compteur5}cel{$compteur}"]}'></td>";
+                        echo "<td id='lig{$compteur5}cel{$compteur}' ><input type='text' name='inputlig{$compteur5}cel{$compteur}'  class='Case' value='{$data["inputlig{$compteur5}cel{$compteur}"]}'></td>";
                     }
                 }
                 echo  "</tr>";

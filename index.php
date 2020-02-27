bd541f0b41204ca8b602d55a53d6a83a604d982c
Kaarththigan <48351119+EaaswaralingamKaarththigan@users.noreply.github.com>
2020-02-27 16:13:04 +0100

    Remise Controler

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require "controler/adminControler.php";
 require "controler/shiftEndControler.php";
 require "controler/todoListControler.php";
 require "controler/drugControler.php";
+require "controler/remiseControler.php";
 
 $action = $_GET['action'];
 
@@ -20,6 +21,8 @@ switch ($action)
         break;
     case 'drugs':
         drugHomePage();
+    case 'remise':
+        remisewrite();
         break;
     default: // unknown action
         require_once 'view/home.php';
